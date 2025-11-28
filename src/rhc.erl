8bc43fa50b89b42767f9adb91deac1933ec389ea
Martin Sumner <martin.sumner@adaptip.co.uk>
2025-11-28 12:59:16 +0000

    List Buckets may return error

--- src/rhc.erl
+++ src/rhc.erl
@@ -863,7 +863,7 @@ aae_object_stats(Rhc, BucketAndType, KeyRange, ModifiedRange) ->
 %% leveled_so parallel store.  A minimum n_val can be passed if known.  If
 %% there are buckets (with keys) below the minimum n_val they may not be
 %% detecting in the query.  Will default to 1.
--spec aae_list_buckets(rhc()) -> {ok, list(maybe_bucket())}.
+-spec aae_list_buckets(rhc()) -> {ok, list(maybe_bucket())}|{error, any()}.
 aae_list_buckets(Rhc) ->
     Url = lists:flatten([root_url(Rhc), "aaebucketlist"]),
     aae_list_buckets(Rhc, Url).
